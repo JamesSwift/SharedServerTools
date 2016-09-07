a70feb5163a84fdfa0156af9928f40b33bbba5f9
root <root@swd-1.swiftwebdevelopment.com>
2016-09-07 08:34:57 +0100

    Initial commit.

---
+++ add-website.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+echo "Creating a new website."
+echo "Before starting, ensure that the domain you are about to setup points to the current server."
+read -rsp $"Press any key to continue..." -n 1 key
+echo ""
+echo "Please enter the domain name for this new website:"
+read domain
+
+echo "Please enter the a username to use for the new website:"
+read username
+
+adduser ${username}
+mkdir /home/${username}/www
+cd /home/${username}/www
+
+echo ""
+
+echo "Adding www-data to group $username (to allow nginx to read the static files)"
+usermod -aG ${username} www-data
+
+echo "Setting up blank git repo in www directory:"
+git init
+git config --local receive.denyCurrentBranch ignore
+
+
+echo "Adding hook to auto checkout pushed commits:"
+cp /root/templates/git-hook.template .git/hooks/post-receive
+sed -i "s/__USERNAME__/${username}/g" .git/hooks/post-receive
+chmod +x .git/hooks/post-receive
+chown -R ${username}.${username} ./
+
+echo "Creating nginx website config file in /etc/nginx/sites-available/${domain}"
+cp /root/templates/nginx-website.template /etc/nginx/sites-available/${domain}
+sed -i "s/__USERNAME__/${username}/g" /etc/nginx/sites-available/${domain}
+sed -i "s/__DOMAIN__/${domain}/g" /etc/nginx/sites-available/${domain}
+
+
+echo "Linking nginx website config to /etc/nginx/sites-enabled/${domain}"
+ln -s /etc/nginx/sites-available/${domain} /etc/nginx/sites-enabled/${domain}
+
+
+echo "Creating php-fpm pool config file in /etc/php/7.0/pfm/pool.d/${username}.conf"
+cp /root/templates/fpm-pool.template /etc/php/7.0/fpm/pool.d/${username}.conf
+sed -i "s/__USERNAME__/${username}/g" /etc/php/7.0/fpm/pool.d/${username}.conf
+
+
+echo "Reloading php-fpm configuration:"
+service php7.0-fpm reload
+echo "Reloading nginx configuration:"
+service nginx reload
+
+
+echo "Obtaining ssl certificate:"
+certbot-auto certonly -a webroot --webroot-path=/home/${username}/www -d ${domain}
+
+
+echo "Installing certificate:"
+sed -i "s/__SSL_DOMAIN__/${domain}/g" /etc/nginx/sites-available/${domain}
+sed -i "s/#__COMMENT__//g" /etc/nginx/sites-available/${domain}
+
+
+echo "Reloading nginx:"
+service nginx reload
+
+
+echo "New website has been added."
+
+
---
+++ remove-website.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "Enter the username of the website you wish to delete:"
+read username
+echo "Enter the domain of the website you wish to delete:"
+read domain
+
+rm /etc/nginx/sites-enabled/$domain
+rm /etc/nginx/sites-available/$domain
+rm /etc/php/7.0/fpm/pool.d/${username}.conf
+service php7.0-fpm restart
+service nginx restart
+deluser www-data $username
+deluser $username
+rm -rf /home/$username
+
