0e73d89c7175352b5e24ec86e0e73ae5741c7703
James Swift <me@james-swift.com>
2017-05-21 23:43:36 +0100

    Added one-filesystem to backup command

--- tools/rsync-backup-fs.sh
+++ tools/rsync-backup-fs.sh
@@ -10,6 +10,6 @@ echo Starting rsync backup task
 echo
 echo Backing up to: $backup_location
 echo 
-rsync -aAXHzv --rsync-path="rsync --fake-super" --ignore-errors --numeric-ids --delete --exclude={"/var/lib/lxcfs/*","/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/lost+found"} / $backup_location
+rsync -aAxXHzv --rsync-path="rsync --fake-super" --ignore-errors --numeric-ids --delete --exclude={"/var/lib/lxcfs/*","/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/lost+found"} / $backup_location
 echo 
 echo Backup Complete
