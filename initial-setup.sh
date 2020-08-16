2c91e61ad102634b17d89de9f3979b2092161c0e
James Swift <me@james-swift.com>
2020-08-16 22:18:22 +0100

    Fix typo

--- initial-setup.sh
+++ initial-setup.sh
@@ -482,4 +482,4 @@ echo
 echo
 echo "You can re-run this setup file at any time to alter your configuration."
 echo
-echo "If your server came with the default 'ubuntu' user, don't forget to remove it or change it's password.
+echo "If your server came with the default 'ubuntu' user, don't forget to remove it or change it's password."
